85d26a751815b29307bd8e2134f83b42ae67aaba
RONNY <mboyaronny41@gmail.com>
2025-12-23 22:02:39 +0300

    Building GET API Code Along

--- server/models.py
+++ server/models.py
@@ -14,8 +14,10 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 
 
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = "games"
+
+    serialize_rules = ('-reviews.game',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -31,8 +33,10 @@ class Game(db.Model):
         return f"<Game {self.title} for {self.platform}>"
 
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = "reviews"
+
+    serrializqe_rules = ('-game.reviews', '-user.reviews')
 
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
@@ -50,8 +54,15 @@ class Review(db.Model):
         return f"<Review ({self.id}) of {self.game}: {self.score}/10>"
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = "users"
+
+    serialize_rules = ('-reviews.user',)
+
+    users = association_proxy("reviews", "users", 
+                              creator=lambda user_obj: Review(user=user_obj))
+
+
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
